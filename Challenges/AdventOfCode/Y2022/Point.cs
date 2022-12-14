7345207a5b69502d6fbb4fdc13dd97cb0ff1edea
ElectroYan <electronicyan@gmail.com>
2022-12-14 17:23:56 +0100

    Revert "Revert "Add Day 14""
    This reverts commit a45c283e3a7c4f95051fee61d450ba81e298578c.

---
+++ Challenges/AdventOfCode/Y2022/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.AdventOfCode.Y2022 {
+    public struct Point {
+        public int x;
+        public int y;
+        public static bool operator ==(Point c1, Point c2) {
+            return c1.x == c2.x && c1.y == c2.y;
+        }
+        public static bool operator !=(Point c1, Point c2) {
+            return c1.x != c2.x || c1.y != c2.y;
+        }
+        public static Point operator +(Point c1, Point c2) {
+            return new Point(c1.x + c2.x, c1.y + c2.y);
+        }
+        public static Point operator -(Point c1, Point c2) {
+            return new Point(c1.x - c2.x, c1.y - c2.y);
+        }
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString() {
+            return $"({x},{y})";
+        }
+    }
+}
