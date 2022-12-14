43371ac1bfbb0c9b2ef58010df499480df9581b7
ElectroYan <electronicyan@gmail.com>
2022-12-14 17:21:04 +0100

    Add Day 14

--- Challenges/AdventOfCode/InputManager.cs
+++ Challenges/AdventOfCode/InputManager.cs
@@ -9,11 +9,11 @@ namespace Challenges.AdventOfCode {
                 Directory.CreateDirectory("Input");
 
             if (File.Exists($"Input/{year}-{day}.txt"))
-                return File.ReadAllText($"Input/{year}-{day}.txt");
+                return File.ReadAllText($"Input/{year}-{day}.txt").Trim('\n');
             
             var client = GetClientWithCookies();
             var data = client.DownloadData($"https://adventofcode.com/{year}/day/{day}/input");
-            var input = Encoding.UTF8.GetString(data);
+            var input = Encoding.UTF8.GetString(data).Trim('\n');
             File.WriteAllText($"Input/{year}-{day}.txt", input);
             return input.Trim('\n');
         }
