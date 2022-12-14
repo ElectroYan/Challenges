9d0128bf9edb47191a962591926d51f5d584daa3
ElectroYan <electronicyan@gmail.com>
2022-12-14 17:23:08 +0100

    Revert "Add Day 9"
    This reverts commit 59a2f2aa246eaac0c02acfdcc749d21084cd689c.

--- Challenges/AdventOfCode/Y2022/Day09.cs
+++ Challenges/AdventOfCode/Y2022/Day09.cs
@@ -6,69 +6,12 @@
         public Day09(string input) : base(input) {
         }
 
-        class Rope {
-            public enum Direction {
-                U, D, L, R
-            }
-            Dictionary<Direction, Point> directions = new Dictionary<Direction, Point> {
-                {Direction.U, new Point(0,1) },
-                {Direction.D, new Point(0,-1) },
-                {Direction.L, new Point(-1,0) },
-                {Direction.R, new Point(1,0) },
-            };
-
-
-
-            //public Point Head = new Point(0, 0);
-            //public Point Tail = new Point(0, 0);
-
-            public HashSet<Point> Positions = new HashSet<Point>();
-
-            public List<Point> Knots = new List<Point>();
-            public Rope(int knots) {
-                Positions.Add(new Point(0, 0));
-                for (int i = 0; i < knots; i++) {
-                    Knots.Add(new Point(0, 0));
-                }
-            }
-
-            public void Move(Direction direction) {
-                Knots[0] = Knots[0] + directions[direction];
-
-                for (int i = 1; i < Knots.Count; i++) {
-                    var diff = Knots[i - 1] - Knots[i];
-                    if (Math.Abs(diff.x) > 1 && Math.Abs(diff.y) > 1) {
-                        Knots[i] = Knots[i] + new Point(Math.Sign(diff.x) * (Math.Abs(diff.x) - 1), Math.Sign(diff.y) * (Math.Abs(diff.y) - 1));
-                    } else if (Math.Abs(diff.x) > 1) {
-                        Knots[i] = Knots[i] + new Point(Math.Sign(diff.x) * (Math.Abs(diff.x) - 1), diff.y);
-                    } else if (Math.Abs(diff.y) > 1) {
-                        Knots[i] = Knots[i] + new Point(diff.x, Math.Sign(diff.y) * (Math.Abs(diff.y) - 1));
-                    }
-
-                }
-                Positions.Add(Knots.Last());
-            }
-        }
-
-
         public override string Level1() {
-            return LevelN(2) + "";
+            return "";
         }
+
         public override string Level2() {
-            return LevelN(10) + "";
+            return "";
         }
-
-        public int LevelN(int knots) {
-            Rope r = new Rope(knots);
-            foreach (var item in InputList) {
-                var direction = (Rope.Direction)Enum.Parse(typeof(Rope.Direction), item[0] + "");
-                var count = int.Parse(item.Split(' ')[1]);
-                for (int i = 0; i < count; i++) {
-                    r.Move(direction);
-                }
-            }
-            return r.Positions.Count;
-        }
-
     }
 }
